7be373cc38e2a52be637d8532feb12907bd8faeb
neha <nehasorout29@gmail.com>
2022-05-23 16:48:53 +0530

    initial commit

--- finalform/src/App.js
+++ finalform/src/App.js
@@ -3,31 +3,10 @@ import './App.css';
 import FinalFormInput from './Components/FinalFormInput';
 
 function App() {
-  const onSubmit =(e)=>{
-debugger;
-  };
-
-  const validate = (e)=>{
-    debugger
-  };
-  
-  return (
-    <div className="App">
-      <h1>React Final Form</h1>
-      <Form
-    onSubmit={onSubmit}
-    validate={validate}
-    render={({ handleSubmit }) => (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>First Name</label>
-          <Field name="firstName" component="input" placeholder="First Name" />
-        </div>
-        
-        <button type="submit">Submit</button>
-      </form>)}/>
-    </div>
+  return(
+<>
+<FinalFormInput/>
+</>
   )
 }
-
 export default App;
--- finalform/src/Components/FinalFormInput.js
+++ finalform/src/Components/FinalFormInput.js
@@ -1,43 +1,96 @@
 import React from 'react'
+import { render } from 'react-dom'
 import { Form, Field } from 'react-final-form'
+import './FinalFormInput.css'
+const onSubmit = ( values) => {
+  console.log(values);
+ 
+}
 
 export default function FinalFormInput() {
-   
+
   return (
-   <>
-     <Form
-    // onSubmit={onSubmit}
-    // validate={validate}
-    render={({handleSubmit})=> (
-    <form onSubmit={handleSubmit}>
-         <div>
-          <label>First Name</label>
-          <Field name="fName" component="input" placeholder="First Name" />
-        </div>
-         <div>
-          <label>Last Name</label>
-          <Field name="lName" component="input" placeholder="Last Name" />
-        </div>
-         <div>
-          <label>Organisation Name</label>
-          <Field name="orgName" component="input" placeholder="Organisation Name" />
-        </div>
-         <div>
-          <label>Email</label>
-          <Field name="email" component="input" placeholder="Email Address" />
-        </div>
-         <div>
-          <label>Phone Number</label>
-          <Field name="phone" component="input" placeholder="Phone Number" />
-        </div>
-        <button type="submit">Submit</button>
-
-    </form>
-
-    )
-    }/>
-     
-   </>
-
+    <div>
+      <h1>React Final Form</h1>
+       <Form
+      onSubmit={onSubmit}
+      validate={values => {
+        const errors = {}
+        if (!values.fname) {
+          errors.fname = 'Required'
+        }
+        if (!values.lname) {
+          errors.lname = 'Required'
+        }
+        if (!values.orgname) {
+          errors.orgname = 'Required'
+        }
+        if (!values.email) {
+          errors.email = 'Required'
+        }
+        if (!values.phone) {
+          errors.phone = 'Required'
+        } 
+        
+        return errors
+      }}
+      render={({ handleSubmit, form, submitting, values }) => (
+        <form onSubmit={handleSubmit}>
+          <Field name="fname">
+            {({ input, meta }) => (
+              <div className='form-control'>
+                <label>First Name</label>
+                <input {...input} type="text"  />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field name="lname">
+            {({ input, meta }) => (
+              <div className='form-control'>
+                <label>Last Name</label>
+                <input {...input} type="text"  />
+                {meta.error&&meta.touched&&<span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field name="orgname">
+            {({ input, meta }) => (
+              <div className='form-control'>
+                <label>Organisation Name</label>
+                <input {...input} type="text"  />
+                {meta.error&&meta.touched&&<span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field name="email">
+            {({ input, meta }) => (
+              <div className='form-control'>
+                <label>Email</label>
+                <input {...input} type="email" />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field name="phone">
+            {({ input, meta }) => (
+              <div className='form-control'>
+                <label>Phone Number</label>
+                <input {...input} type="text" />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <div className="buttons">
+            <button type="submit" disabled={submitting}>
+              Submit
+            </button>
+            
+          </div>
+         
+        </form>
+      )}
+    />
+    </div>
   )
 }
