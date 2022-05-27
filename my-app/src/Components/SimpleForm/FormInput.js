52f026f9cbf6d3b3c983622971094f182b1c4728
neha <nehasorout29@gmail.com>
2022-05-27 12:00:40 +0530

    initial commit

--- my-app/src/Components/SimpleForm/FormInput.js
+++ my-app/src/Components/SimpleForm/FormInput.js
@@ -52,7 +52,7 @@ const submitHandel=(event) =>{
     setEmailError("Required");
   }
   else if (
-    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)
   ) {
   setEmailError ('Invalid email address') ;
   }
